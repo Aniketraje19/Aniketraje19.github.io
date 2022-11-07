f6855eb777da349b6dd601487c14d238f67b3071
Aniketraje19 <116674837+Aniketraje19@users.noreply.github.com>
2022-11-07 22:14:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+
+
+
+
+function dbs1(){
+    var dbs = document.getElementById("dbs");
+    dbs.src = "images/models/dbs.png";
+}
+
+function dbs2(){
+    var dbs = document.getElementById("dbs");
+    dbs.src = "images/models/dbs2.png";
+}
+
+
+function v1(){
+    var vantage = document.getElementById("vantage");
+    vantage.src = "images/models/vantage2.png";
+}
+
+function v2(){
+    var vantage = document.getElementById("vantage");
+    vantage.src = "images/models/vantage3.png";
+}
+function v3(){
+    var vantage = document.getElementById("vantage");
+    vantage.src = "images/models/vantage4.png";
+}
+
+function v4(){
+    var vantage = document.getElementById("vantage");
+    vantage.src = "images/models/vantage5.png";
+}
+
+function v5(){
+    var vantage = document.getElementById("vantage");
+    vantage.src = "images/models/vantage6.png";
+}
+
+function v6(){
+    var vantage = document.getElementById("vantage");
+    vantage.src = "images/models/vantage.png";
+}
+
+function db111(){
+    var db11 = document.getElementById("db11");
+    db11.src = "images/models/db11.png";
+}
+
+function db112(){
+    var db11 = document.getElementById("db11");
+    db11.src = "images/models/db112.png";
+}
+function db113(){
+    var db11 = document.getElementById("db11");
+    db11.src = "images/models/db113.png";
+}
+
+function dbx1(){
+    var dbx = document.getElementById("dbx");
+    dbx.src = "images/models/dbx2.png";
+}
+
+function dbx2(){
+    var dbx = document.getElementById("dbx");
+    dbx.src = "images/models/dbx.png";
+}
+
+
+
+
+
+
